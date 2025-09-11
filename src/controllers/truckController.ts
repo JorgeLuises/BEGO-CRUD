a8a57684c0b6ac40a82bb9ced7d3bb6241de49d9
JorgeLuises <161666613+JorgeLuises@users.noreply.github.com>
2025-09-11 14:54:58 -0600

    Termino de endpoints para trucks

--- src/controllers/truckController.ts
+++ src/controllers/truckController.ts
@@ -22,3 +22,42 @@ export async function crearTruck (req: Request, res: Response) {
         return res.status(500).json({ErrorMessage: "Error al crear el registro", error});
     }
 }
+
+export async function obtenerTrucks (req: Request, res: Response) {
+    // ===== Obteniendo todos los trucks ===== //
+    try {
+        const trucks = await Truck.find();
+        return res.status(200).json({ Message: "Mostrando los registros", trucks });
+    } catch (error) {
+        return res.status(500).json({ ErrorMessage: "Error al obtener los registros", error });
+    }
+}
+
+export async function actualizarTruck (req: Request, res: Response) {
+    const { id } = req.params;
+    const { year, color, plates } = req.body;
+
+    if (!year || !color || !plates) return res.status(400).json({ ErrorMessage: "Faltan campos obligatorios por rellenar" });
+
+    // ===== Actualizando un truck por su ID ===== //
+    try {
+        const updatedTruck = await Truck.findByIdAndUpdate(id, { year, color, plates }, {new: true});
+        if (!updatedTruck) return res.status(404).json({ ErrorMessage: "Registro no encontrado", id });
+        return res.status(200).json({ Message: "Registro actualizado exitosamente", updatedTruck });
+    } catch (error) {
+        return res.status(500).json({ ErrorMessage: "Error al actualizar el registro", error });
+    }
+}
+
+export async function borrarTruck (req: Request, res: Response) {
+    const { id } = req.params;
+
+    // ===== Borrando un truck por su ID ===== //
+    try {
+        const deletedTruck = await Truck.findByIdAndDelete(id);
+        if (!deletedTruck) return res.status(404).json({ ErrorMessage: "El registro no pudo ser encontrado", id });
+        return res.status(200).json({ Message: "Regsitro eliminado de la base de datos exitosamente", deletedTruck });
+    } catch (error) {
+        return res.status(500).json({ ErrorMessage: "Error al eliminar este registro", error });
+    }
+}
